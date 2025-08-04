626c0a67b4033fa1ad742982620ffe8173b7d9bc
praveen007 <praveenbharadwaj_m@yahoo.co.in>
2025-08-04 16:17:43 +0530

    Vny sn (#5)
    * life-at-poddar
    
    ---------
    
    Co-authored-by: snvinay <snvinays@gmail.com>

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -15,5 +15,9 @@ export const routes: Routes = [
   {
     path: 'products',
     loadComponent: () => import('./components/products/products.component').then(m => m.ProductsComponent)
+  },
+  {
+    path: 'life-at-poddar',
+    loadComponent: () => import('./features/careers/life-at-poddar.component').then(m => m.LifeAtPoddarComponent)
   }
 ];
